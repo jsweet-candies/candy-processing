33e2650ff942aff3356e40f25690e7ed840d94d4
Louis Grignon <louis.grignon@gmail.com>
2018-06-26 10:18:17 +0200

    processing ts def

---
+++ src/main/resources/META-INF/resources/typings/processing/3.3.7-SNAPSHOT/processing.d.ts
@@ -0,0 +1,16 @@
+interface PFont {    
+}
+
+declare class PImage {
+    
+}
+
+declare class PConstants {
+    static POLYGON: number;
+    static SCREEN: number;
+    static LINES: number;
+}
+
+declare class PApplet extends PConstants {
+    
+}
